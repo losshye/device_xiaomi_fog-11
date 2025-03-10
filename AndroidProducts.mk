acc3fa59ed9472cef223768b7057370aa0e732bb
Dwyor Tmx <akunfreefire15417@gmail.com>
2025-03-10 02:54:07 +0000

    nitrogen time
    Signed-off-by: Dwyor Tmx <akunfreefire15417@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_fog.mk
+    $(LOCAL_DIR)/nitrogen_fog.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_fog-user \
-    lineage_fog-userdebug \
-    lineage_fog-eng
+    nitrogen_fog-user \
+    nitrogen_fog-userdebug \
+    nitrogen_fog-eng
