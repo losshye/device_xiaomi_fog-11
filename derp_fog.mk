4b247ecbcf2cb88a95e4c26566e2c26e99b172b6
Loosiee <akunfreefire15417@gmail.com>
2025-03-13 20:55:45 +0700

    Update and rename nitrogen_fog.mk to derp_fog.mk

--- derp_fog.mk
+++ derp_fog.mk
@@ -9,12 +9,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/nitrgoen/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
-PRODUCT_NAME := nitrogen_fog
+PRODUCT_NAME := derp_fog
 PRODUCT_DEVICE := fog
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
